06d69dcd4831e46ba6ecdef0608fb5ed6cb6389d
Maltazarul221 <makrusu80@gmail.com>
2025-09-04 21:28:07 +0100

    commit

--- individual_work/1.general_recap.py
+++ individual_work/1.general_recap.py
@@ -13,3 +13,25 @@ print("Hey John!")
 print(type("Hey John!"))
 print(type(x))
 print(type(y))
+
+#they don't need to be declared with any particular type, and you can change them after they have been assigned
+
+x = 4
+x = "Sally"
+
+print(x)
+
+#Casting
+#if you want to specify the data type of variable , this can be done by casting
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+
+
+
