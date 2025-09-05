21f1bbc972c8f267b6555d4bca328b034f2e90bd
Maltazarul221 <makrusu80@gmail.com>
2025-09-05 16:04:01 +0100

    1

--- individual_work/1.general_recap.py
+++ individual_work/1.general_recap.py
@@ -32,6 +32,24 @@ print(x)
 print(y)
 print(z)
 
+#Variable names
+#can have a short name ( x, y ) or a more descriptive name ( age, carname , total_value )
+#Rules for Python variables:
+
+#A variable name must start with a letter or the underscore character
+#A variable name cannot start with a number
+#A variable name can only contain alphanumeric characters and underscores (A-z, 0-9, and _ )
+#Variable names are case-sensitive (age, Age and AGE are three different variables)
+#A variable name cannot be any of the Python keywords.
+
+# Examples of variables:
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
 
 
 
